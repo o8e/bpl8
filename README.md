888fd01ed3305dd4ace0368dfdab9a0b164bd514
Ollie Tribe <olliet@tomandco.co.uk>
2015-04-22 13:39:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## bpl8
+My personal boilerplate and build system, decent HTML5 base with normalize, modernizr and bower and gulp already configured for styles and scripts. I usually use Foundation sass, so I've just imported the grid component initially.
+
+###### Installation
+In the unlikely event that you decide you wish to use this as a base for your project, here's a quick run down of what you need to do.
+
+1. Clone this repo `git clone https://github.com/trybe/bpl8.git` or whatever
+2. In the project directory, run `bower install` and `npm install` to grab dependencies
+3. Run `gulp`, the default task will watch and compile.
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+      sass = require('gulp-ruby-sass'),
+      concat = require('gulp-concat'),
+      uglify = require('gulp-uglify');
+
+gulp.task('sass', function() {
+  return sass('src/scss/style.scss')
+  .on('error', function(err) { console.error('Error!', err.message); })
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('vendor', function() {
+  return gulp.src('src/vendor/modernizr/*.js')
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('default', ['sass','js','vendor','watch']);
