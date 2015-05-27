8e3ba74a56195a78b0ddada539e4d4ca36837e8e
Ollie Tribe <olliet@tomandco.co.uk>
2015-05-27 12:29:49 +0100

    Watch for JS

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ gulp.task('sass', function() {
 
 gulp.task('watch', function() {
   gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/js/**/*.scss', ['js']);
 });
 
 gulp.task('vendor', function() {
